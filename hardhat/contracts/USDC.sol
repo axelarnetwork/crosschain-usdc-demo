5f85480bcab37d49b15bfebca84f784078caa029
npty <nptytn2@gmail.com>
2022-09-09 18:40:38 +0700

    feat: add setMinter and setBurner function to usdc

--- hardhat/contracts/USDC.sol
+++ hardhat/contracts/USDC.sol
@@ -6,19 +6,27 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract USDC is ERC20Burnable, AccessControl {
     // Create a new role identifier for the minter role
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
-    constructor(address minter, address burner) ERC20("Circle USDC", "USDC") {
+    constructor() ERC20("Circle USDC", "USDC") {
         _mint(msg.sender, 1000000000000000000000000);
-        // Grant the minter role to a specified account
-        _setupRole(MINTER_ROLE, minter);
-        // Grant the burner role to a specified account
-        _setupRole(BURNER_ROLE, burner);
+        _setupRole(ADMIN_ROLE, msg.sender);
     }
 
     function decimals() public view virtual override returns (uint8) {
         return 6;
+    }
+
+    function setMinterRole(address minter) public {
+        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
+        _setupRole(MINTER_ROLE, minter);
+    }
+
+    function setBurnerRole(address burner) public {
+        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
+        _setupRole(BURNER_ROLE, burner);
     }
 
     function mint(address to, uint256 amount) public {
