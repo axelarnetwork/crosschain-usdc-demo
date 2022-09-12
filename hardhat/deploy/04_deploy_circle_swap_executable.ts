bfb04783834c62c4f867c2f58e81876d9b44ad5e
npty <nptytn2@gmail.com>
2022-09-12 19:51:28 +0700

    feat: add deploy script for CircleSwapExecutable contract

---
+++ hardhat/deploy/04_deploy_circle_swap_executable.ts
@@ -0,0 +1,36 @@
+import { DeployFunction, DeployResult } from "hardhat-deploy/dist/types";
+import { deployments } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { GAS_RECEIVER, GATEWAY } from "../constants/address";
+import { Chain } from "../constants/chains";
+
+const deploy: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deploy } = hre.deployments;
+  const { deployer } = await hre.getNamedAccounts();
+  const chainName = hre.network.name as Chain;
+  const usdc = await deployments.get("USDC");
+  const circleBridge = await deployments.get("CircleBridge");
+  const args = [
+    GATEWAY[chainName],
+    GAS_RECEIVER[chainName],
+    usdc.address,
+    circleBridge.address,
+  ];
+  const result: DeployResult = await deploy("CircleSwapExecutable", {
+    from: deployer,
+    args,
+    log: true,
+  });
+
+  await hre
+    .run("verify:verify", {
+      address: result.address,
+      constructorArguments: args,
+    })
+    .catch((e) => console.log(e.message));
+};
+
+deploy.tags = ["CircleSwapExecutable"];
+deploy.dependencies = ["USDC", "CircleBridge"];
+
+export default deploy;
