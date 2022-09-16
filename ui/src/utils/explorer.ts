8edd8acb2ce407451dbea9475dead60d4ab08f01
npty <nptytn2@gmail.com>
2022-09-16 14:38:38 +0700

    fix: explorer links

--- ui/src/utils/explorer.ts
+++ ui/src/utils/explorer.ts
@@ -3,10 +3,8 @@ import { ChainId } from "types/chain";
 export const getAddressLink = (chainId: ChainId, address: string) => {
   if (chainId === ChainId.AVALANCHE) {
     return `https://testnet.snowtrace.io/address/${address}`;
-  } else if (chainId === ChainId.ETHEREUM) {
-    return `https://ropsten.etherscan.io/address/${address}`;
-  } else if (chainId === ChainId.MOONBEAM) {
-    return `https://moonbase.moonscan.io/address/${address}`;
+  } else if (chainId === ChainId.FANTOM) {
+    return `https://testnet.ftmscan.com/address/${address}`;
   }
   return "";
 };
@@ -14,10 +12,8 @@ export const getAddressLink = (chainId: ChainId, address: string) => {
 export const getTxLink = (chainId: ChainId, hash: string) => {
   if (chainId === ChainId.AVALANCHE) {
     return `https://testnet.snowtrace.io/tx/${hash}`;
-  } else if (chainId === ChainId.ETHEREUM) {
-    return `https://ropsten.etherscan.io/tx/${hash}`;
-  } else if (chainId === ChainId.MOONBEAM) {
-    return `https://moonbase.moonscan.io/tx/${hash}`;
+  } else if (chainId === ChainId.FANTOM) {
+    return `https://testnet.ftmscan.com/tx/${hash}`;
   }
   return "";
 };
