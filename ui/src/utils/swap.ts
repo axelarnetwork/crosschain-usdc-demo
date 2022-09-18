4b98f81314c572e3b6af4d00205830a2842d3252
npty <nptytn2@gmail.com>
2022-09-18 14:26:12 +0700

    chore: fix swap function

--- ui/src/utils/swap.ts
+++ ui/src/utils/swap.ts
@@ -12,21 +12,5 @@ export function requiredSwapDest(
   destToken: Token,
   destChain: SquidChain
 ) {
-  // Always required swap when the output token is custom erc20 token.
-  if (!destToken.crosschain) return true;
-
-  // If srcToken is not a cross-chain token, then the destination chain will receive the defaultCrosschainToken.
-  // As a result, it requires swap if the destToken is not a defaultCrosschainToken.
-  if (
-    !srcToken.crosschain &&
-    destChain.defaultCrosschainToken !== destToken.address
-  )
-    return true;
-
-  // If srcToken is a cross-chain token, then the destination chain will receive the same cross-chain token.
-  // As a result, it requires swap if both srcToken and destToken symbol are not equal. (destToken is a cross-chain token here because it passes the first check.)
-  if (srcToken.crosschain && srcToken.symbol !== destToken.symbol) return true;
-
-  // Otherwise, it doesn't require swap.
-  return false;
+  return !destToken.crosschain;
 }
